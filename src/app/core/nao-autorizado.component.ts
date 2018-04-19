f32bb258880a43656e45e3b51f64f57fb0fe07d4
Antonio Luciano Lima da Silva <luclimasilva23@gmail.com>
2018-04-19 15:29:03 -0300

    Quarto commit

--- src/app/core/nao-autorizado.component.ts
+++ src/app/core/nao-autorizado.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nao-autorizado',
   template: `
   <div class="container">
-    <h1 class="text-center">Acesso negado!</h1>
+    <h1 class="text-center">Acesso negado!!!</h1>
   </div>
   `,
   styles: []
